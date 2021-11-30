7306bc4c8de4490877beb23329924e02c624d21b
Chetan Bhogade <chetan.bhogade3899@gmail.com>
2021-11-30 12:58:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Container,
+  Grid,
+  Typography,
+} from "@mui/material";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [moviesData, setMoviesData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/get-movies")
+      .then((response) => {
+        console.log("response from api: - ", response);
+        setMoviesData(response.data.data);
+      })
+      .catch((error) => {
+        console.log("got an error: - ", error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Grid container>
+        {moviesData.map((item) => {
+          return (
+            <Grid key={item.id} item xs={4}>
+              <Container style={{ padding: "25px" }}>
+                <Card sx={{ minWidth: 275 }}>
+                  <CardContent>
+                    <Typography variant="h5" component="div">
+                      {item.name}
+                    </Typography>
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                      {item.releaseDate}
+                    </Typography>
+                    <Typography variant="body2">
+                      rating: - {item.rating}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">Learn More</Button>
+                  </CardActions>
+                </Card>
+              </Container>
+            </Grid>
+          );
+        })}
+      </Grid>
     </div>
   );
 }
